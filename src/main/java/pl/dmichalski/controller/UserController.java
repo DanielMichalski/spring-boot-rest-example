6001d25cd7e492e4013a51dc6402896e87a24e72
Daniel Michalski <daniel.michalski@marviq.com>
2020-07-31 21:58:31 +0200

    Value testing

--- src/main/java/pl/dmichalski/controller/UserController.java
+++ src/main/java/pl/dmichalski/controller/UserController.java
@@ -3,6 +3,7 @@ package pl.dmichalski.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,9 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    @Value("${lwt.application.five-minute-jobs}")
+    private boolean fiveMinuteJobsEnabled;
+
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -24,6 +28,7 @@ public class UserController {
 
     @RequestMapping(method = RequestMethod.GET)
     public List<User> findAllUsers() {
+        System.out.println(fiveMinuteJobsEnabled);
         return userRepository.findAll();
     }
 
